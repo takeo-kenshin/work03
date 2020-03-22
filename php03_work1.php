ab9655c4fdd5b0d0efc1dab3efa244fc474dd790
竹尾憲真 <viento3009@gmail.com>
2020-03-22 05:55:09 +0000

    PHP/Laravel03の課題提出

---
+++ php03_work1.php
@@ -0,0 +1,8 @@
+<?php
+$name='TAKEO';
+if($name == 'TAKEO'){
+    echo '私は あなたの名前 です';
+}
+    else{
+    echo 'あなたの名前ではありません';
+}
---
+++ php03_work3.php
@@ -0,0 +1,5 @@
+<?php
+$fruits=array('apple','banana','peach','orange','grape');
+foreach($fruits as $fruit){
+    echo $fruit;
+}
